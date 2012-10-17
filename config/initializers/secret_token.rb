72dd3d5065447304f9da5be0e8b8f5249f376eec
HeeL <parizhskiy@gmail.com>
2012-10-17 09:09:39 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LonglLatSearch::Application.config.secret_token = '4418955e6033cb4639698c38dfd290f1c2ffd07e2c6746139a40343ac0b2c2b36cca11ed028bc5de409df668f3984c4396e644f984734333e3448c80497352e1'
